e14abcf1446c4b3e7a41b567aeea4b17d857c65b
246859 <2633565580@qq.com>
2023-11-28 15:48:50 +0800

    test(testutil): add test randkv util

--- testutil.go
+++ testutil.go
@@ -1,6 +1,9 @@
 package riverdb
 
 import (
+	"fmt"
+	"github.com/bwmarrin/snowflake"
+	"math/rand"
 	"os"
 	"path/filepath"
 )
@@ -11,7 +14,8 @@ func testDB(option Options) (db *DB, closeDB func() error, err error) {
 	if err != nil {
 		return nil, nil, err
 	}
-	dir := filepath.Join(homeDir, "test_db")
+
+	dir := filepath.Join(homeDir, fmt.Sprintf("test_db_%s", string(testRandKV().testUniqueBytes(10))))
 	err = os.MkdirAll(dir, 0777)
 	if err != nil {
 		return nil, nil, err
@@ -32,4 +36,35 @@ func testDB(option Options) (db *DB, closeDB func() error, err error) {
 		}
 		return nil
 	}, nil
+}
+
+func testRandKV() *randKV {
+	node := testRandNode()
+	return &randKV{node}
+}
+
+type randKV struct {
+	node *snowflake.Node
+}
+
+func (r *randKV) testBytes(ln int) []byte {
+	ln = rand.Intn(ln)
+	key := make([]byte, 0, ln)
+	for i := 0; i < ln; i++ {
+		key = append(key, byte('a'+rand.Int()%26))
+	}
+	return key
+}
+
+// use snowflake to make sure that bytes is unique
+func (r *randKV) testUniqueBytes(ln int) []byte {
+	return append(r.node.Generate().Bytes(), r.testBytes(ln)...)
+}
+
+func testRandNode() *snowflake.Node {
+	node, err := snowflake.NewNode(int64(rand.Int() % 1024))
+	if err != nil {
+		panic(err)
+	}
+	return node
 }
